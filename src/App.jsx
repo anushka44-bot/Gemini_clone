36ac3c0c1bd72c74ce3d982aec59616c87df18c5
Anushka Bag <bag82262@gmail.com>
2026-01-02 09:47:08 +0530

    Sidebar Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default App;
